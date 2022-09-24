cca56cfc45da29b559362572c6461d7ee3bd05a5
hsami-se <eng.sami247@gmail.com>
2022-09-24 06:23:37 -0700

    svm

---
+++ 5-Support Vector Regression (SVR)/Support_Vector_Regression_Sami.py
@@ -0,0 +1,18 @@
+# building a SVR
+# 1. Collecting a training set T = {X, Y}
+# 2. Choose a kernel and it's parameters as well as any regularization needed
+# 3. Form the correlation matrix, K
+# 4. Train your machine, exactly or approximately, to get contraction coeeficents a = {ai}
+# 5. Use those coefficients, create your estimator f(X, a, x*) = y*
+
+
+
+# 1. Collecting a training set T = {X, Y}
+
+# 2. Choose a kernel and it's parameters as well as any regularization needed
+
+# 3. Form the correlation matrix, K
+
+# 4. Train your machine, exactly or approximately, to get contraction coeeficents a = {ai}
+
+# 5. Use those coefficients, create your estimator f(X, a, x*) = y*
